c870c8bc7140e74e3ea7832f99297cd35ecca6ab
JohnDaleRizare <161821205+JohnDaleRizare@users.noreply.github.com>
2024-03-01 11:48:22 +0000

    Fix for the image host

--- chickencoindapp/next.config.mjs
+++ chickencoindapp/next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['w7.pngwing.com'], // Add the hostname here
+    },
+  };
+  
+  export default nextConfig;
+  
