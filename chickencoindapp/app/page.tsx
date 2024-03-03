84ba359439f98286ebf4ccb994446af3c414da7a
JohnDaleRizare <161821205+JohnDaleRizare@users.noreply.github.com>
2024-03-03 16:37:06 +0000

    Many UI Changes and Improvements (Fixing Stuffs)

--- chickencoindapp/app/page.tsx
+++ chickencoindapp/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { BrowserProvider } from "ethers";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { getContract } from "../config";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const [walletKey, setWalletKey] = useState("");
-  const [currentData, setcurrentData] = useState("");
-
+  const [mintingAmount, setMintingAmount] = useState<number>();
+  const [stakingAmount, setStakingAmount] = useState<number>();
   const [processingFunds, setProcessingFunds] = useState(false);
 
   const connectWallet = async () => {
@@ -18,11 +19,6 @@ export default function Home() {
     setWalletKey(accounts[0]);
   };
 
-  // Manok Minting
-  const [mintingAmount, setMintingAmount] = useState<number>();
-  const [submitted, setSubmitted] = useState(false);
-  const [transactionHash, setTransactionHash] = useState("");
-
   const mintCoin = async () => {
     const { ethereum } = window as any;
     const provider = new BrowserProvider(ethereum);
@@ -31,8 +27,6 @@ export default function Home() {
     try {
       const tx = await contract.mint(signer, mintingAmount);
       await tx.wait();
-      setSubmitted(true);
-      setTransactionHash(tx.hash);
     } catch (e: any) {
       const decodedError = contract.interface.parseError(e.data);
       alert(`Minting Failed: ${decodedError?.args}`);
@@ -43,14 +37,11 @@ export default function Home() {
     const inputValue = e.target.value;
     if (!isNaN(Number(inputValue))) {
       setMintingAmount(Number(inputValue));
-      console.log(inputValue);
     } else {
       setMintingAmount(0);
     }
   };
 
-  //Hen House Staking
-  const [stakingAmount, setStakingAmount] = useState<number>();
   const stakeCoin = async () => {
     const { ethereum } = window as any;
     const provider = new BrowserProvider(ethereum);
@@ -59,8 +50,6 @@ export default function Home() {
     try {
       const tx = await contract.stake(stakingAmount);
       await tx.wait();
-      setSubmitted(true);
-      setTransactionHash(tx.hash);
     } catch (e: any) {
       const decodedError = contract.interface.parseError(e.data);
       alert(`Staking Failed: ${decodedError?.args}`);
@@ -71,13 +60,11 @@ export default function Home() {
     const inputValue = e.target.value;
     if (!isNaN(Number(inputValue))) {
       setStakingAmount(Number(inputValue));
-      console.log(inputValue);
     } else {
       setStakingAmount(0);
     }
   };
 
-  //The Big Coop Withdraw
   const withdrawCoin = async () => {
     const { ethereum } = window as any;
     const provider = new BrowserProvider(ethereum);
@@ -87,8 +74,6 @@ export default function Home() {
       setProcessingFunds(true);
       const tx = await contract.withdraw();
       await tx.wait();
-      setSubmitted(true);
-      setTransactionHash(tx.hash);
     } catch (e: any) {
       const decodedError = contract.interface.parseError(e.data);
       alert(`Withdrawal Failed: ${decodedError?.args}`);
@@ -97,7 +82,6 @@ export default function Home() {
     }
   };
 
-  //Import Token
   const importToken = async () => {
     const { ethereum } = window as any;
     const tokenAddress = "0x13dab8d8F442C4aC5E5E3CDC1F36dFe49627c5df";
@@ -118,9 +102,9 @@ export default function Home() {
           },
         },
       });
-      console.log("Token Added Successfully:", wasAdded);
+      console.log("Token is Added:", wasAdded);
     } catch (error) {
-      console.error("Error adding token:", error);
+      console.error("Error", error);
       alert("Failed to add the token. Please try again.");
     }
   };
@@ -128,33 +112,41 @@ export default function Home() {
   return (
     <main
       style={{
+        backgroundImage:
+          "url('https://www.shutterstock.com/image-photo/aguascalientes-mexico-march-27-2018-600nw-1333340810.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        background:
-          "linear-gradient(to bottom, rgba(0, 0, 128, 0.8) 0%, rgba(0, 0, 128, 1) 100%)",
         minHeight: "100vh",
       }}
     >
-      <div style={{ position: 'absolute', top: 130, right: 750 }}>
-        <Image src="https://img.freepik.com/premium-photo/cute-round-thai-chicken-fiery-emblem-our-restaurant_983420-11635.jpg" alt="Token Image" width={50} height={50} />
+      <div className={styles.roundedImage}>
+        <Image
+          src="https://img.freepik.com/premium-photo/cute-round-thai-chicken-fiery-emblem-our-restaurant_983420-11635.jpg"
+          alt="Token Image"
+          width={100}
+          height={100}
+        />
       </div>
-      <h1 style={{ color: "red", fontSize: "36px" }}>
-        Cock-a-doodle-doo! It&apos;s Time for a{" "}
-        <span style={{ color: "yellow" }}>Featherweight Fight</span>
-      </h1>
-      <div style={{ minHeight: "15vh" }}>
+      <h1 className="text-red-500 text-4xl font-bold">
+          Cock-a-doodle-doo! It&apos;s Time for a{" "}
+          <span className="text-yellow-500">Featherweight Fight!</span>
+        </h1>
+      <div style={{ minHeight: "10vh" }}>
         <button
-          onClick={() => {
-            connectWallet();
-          }}
-          className="p-3 bg-green-500 text-white rounded"
-          style={{ marginRight: "10px" }}
+          onMouseEnter={(e) => (e.currentTarget.style.filter = "brightness(120%)")}
+          onMouseLeave={(e) => (e.currentTarget.style.filter = "brightness(100%)")}
+          onClick={() => connectWallet()}
+          className="p-3 bg-green-500 text-white rounded mr-4 mb-4"
         >
-          {walletKey !== "" ? `${walletKey} (Connected)` : "Enter the Coop"}
+          {walletKey !== "" ? `${walletKey} (Connected)` : "Enter the Arena"}
         </button>
         <button
+          onMouseEnter={(e) => (e.currentTarget.style.filter = "brightness(120%)")}
+          onMouseLeave={(e) => (e.currentTarget.style.filter = "brightness(100%)")}
           onClick={importToken}
           className="p-3 bg-red-500 text-white rounded"
         >
@@ -162,103 +154,84 @@ export default function Home() {
         </button>
       </div>
       <div style={{ marginBottom: "20px", textAlign: "center" }}>
-        <Image
-          src="https://t3.ftcdn.net/jpg/05/69/02/32/360_F_569023216_r2YOh0ILJSoL4Wsjrv2RqJal61SW20x4.jpg"
-          alt="Jupiter Logo"
-          width={520}
-          height={128}
-        />
+        <div className={styles.roundedImage}>
+          <Image
+            src="https://t3.ftcdn.net/jpg/05/69/02/32/360_F_569023216_r2YOh0ILJSoL4Wsjrv2RqJal61SW20x4.jpg"
+            alt="Sabong"
+            width={520}
+            height={128}
+          />
+        </div>
       </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: "20px",
-        }}
-      >
-        <br />
-        <form>
-          <label style={{ color: "white", fontSize: "18px" }}>
-            {" "}
-            Place Your Bet (Amount to Mint)
-          </label>
-          <br />
-        </form>
+      <div className="flex justify-center items-center flex-col p-4">
+        <label className="text-white text-lg">Place Your Bet (Amount to Mint)</label>
         <input
           type="number"
           value={mintingAmount}
           onChange={(e) => mintAmountChange(e)}
-          style={{ color: "black" }}
+          className="border-2 border-gray-300 p-2 rounded-lg mt-2"
+        />
+        <input
+          type="range"
+          min={0}
+          max={500}
+          value={mintingAmount}
+          onChange={(e) => mintAmountChange(e)}
+          className="mt-2"
         />
         <button
-          onClick={() => {
-            mintCoin();
-          }}
-          className="p-3 bg-green-500 text-white rounded"
+          onMouseEnter={(e) => (e.currentTarget.style.filter = "brightness(120%)")}
+          onMouseLeave={(e) => (e.currentTarget.style.filter = "brightness(100%)")}
+          onClick={() => mintCoin()}
+          className="p-3 bg-green-500 text-white rounded mt-2"
         >
-          {" "}
-          Mint CHICKEN{" "}
+          Mint CHICKEN
         </button>
       </div>
-
-      <br />
-
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: "20px",
-        }}
-      >
-        <form>
-          <label style={{ color: "white", fontSize: "18px" }}>
-            {" "}
-            Train Your Fighters (Amount to Stake)
-          </label>
-          <br />
-        </form>
+      <div className="flex justify-center items-center flex-col p-4">
+        <label className="text-white text-lg">Train Your Fighters (Amount to Stake)</label>
         <input
           type="number"
           value={stakingAmount}
           onChange={(e) => stakeAmountChange(e)}
-          style={{ color: "black" }}
+          className="border-2 border-gray-300 p-2 rounded-lg mt-2"
+        />
+        <input
+          type="range"
+          min={0}
+          max={500}
+          value={stakingAmount}
+          onChange={(e) => stakeAmountChange(e)}
+          className="mt-2"
         />
         <button
-          onClick={stakeCoin}
-          className="p-3 bg-red-500 text-white rounded"
+          onMouseEnter={(e) => (e.currentTarget.style.filter = "brightness(120%)")}
+          onMouseLeave={(e) => (e.currentTarget.style.filter = "brightness(100%)")}
+          onClick={() => stakeCoin()}
+          className="p-3 bg-red-500 text-white rounded mt-2"
         >
-          {" "}
-          Stake CHICKEN{" "}
+          Stake CHICKEN
         </button>
       </div>
-
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "20px",
-        }}
-      >
+      <div className="flex justify-center items-center flex-col p-4">
         {processingFunds ? (
-          <label style={{ color: "white", fontSize: "16px" }}>
+          <label className="text-white text-lg">
             Please wait while processing your funds.
             <br />
             ETA: 1 Minute
           </label>
         ) : (
           <button
-            onClick={withdrawCoin}
+            onMouseEnter={(e) => (e.currentTarget.style.filter = "brightness(120%)")}
+            onMouseLeave={(e) => (e.currentTarget.style.filter = "brightness(100%)")}
+            onClick={() => withdrawCoin()}
             className="p-3 bg-green-500 text-white rounded"
           >
             Collect Your Winnings (Withdraw Staked Chicken + Rewards)
           </button>
         )}
       </div>
-      <p style={{ color: "white", fontSize: "14px", marginTop: "20px" }}>
+      <p className="text-white text-lg mt-4">
         A Revolutionary Shit Coin That Will Hatch Money For You. It&apos;s all a game of CHICKEN at the end of the day.
       </p>
     </main>
